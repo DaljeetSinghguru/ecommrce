f40f0af0354a841d2207d1edb276682f060142e5
daljeetsinghguru <daljeetsinghguru@gmail.com>
2018-08-11 14:26:40 +0530

    submit chnages after disign fix
    submit chnages after disign fix

---
+++ ecommrce/App/controllers/ShopDetailController.js
@@ -0,0 +1,31 @@
+﻿app.controller('ShopDetailCtrl', function ($scope, $http,  $window, $rootScope, ShopService) {
+   
+
+    debugger
+    $scope.$watch(function () { return ShopService.getXxx(); }, function (newValue, oldValue) {
+        debugger
+        if (newValue != null) {
+            //update Controller2's xxx value
+            $scope.xxx = newValue;
+        }
+    }, true);
+
+
+
+    //$http({ method: 'GET', url: 'http://localhost:63988/api/ShopPortalListing'}).
+    //    success(function (data, status, headers, config) {
+            
+    //                        $scope.response = data;
+
+    //                    }).
+    //                    error(function (data, status, headers, config) {
+    //                    });
+
+    //$scope.function = function (data)
+    //{
+    //    debugger
+    //    $scope.data = data;
+    //   // $window.location.href = '/index.html';
+    //}
+
+});
---
+++ ecommrce/App/services/ShopService.js
@@ -0,0 +1,25 @@
+﻿//app.service('ShopService', function () {
+
+//    debugger
+//   //this.xxx = data;
+
+//    this.sendData = function (data) {
+//        this.xxx = data;
+//    };
+//});
+app.service("ShopService", function () {
+    debugger
+    var _xxx = {};
+
+    return {
+        getXxx: function () {
+            debugger
+            return _xxx;
+        },
+        setXxx: function (value) {
+            debugger
+            _xxx = value;
+        }
+    };
+
+});
