b46ba837c9e5791600ad3e1997dae6cedd33fa1d
daljeetsinghguru <daljeetsinghguru@gmail.com>
2018-08-18 12:47:08 +0530

    add category changes
    add category changes

---
+++ ecommrce/App/Admin/Services/CategoryMasterService.js
@@ -0,0 +1,15 @@
+﻿app.service('CategoryMasterService', ['$http',  function ($http) {
+
+
+
+    this.baseURl = "http://localhost:50433/api/";
+
+
+    this.GetCategory = function () {
+        return $http({ method: 'GET', url: this.baseURl + 'CategoryMaster' });
+    }
+    this.InsertRegion = function (Object) {
+        return $http({ method: 'POST', url: this.baseURl + 'Region/InsertUpdateRegion', data: Object });
+    }
+
+}])
--- ecommrce/App/Admin/controllers/CategoryController.js
+++ ecommrce/App/Admin/controllers/CategoryController.js
@@ -1,1 +1,157 @@
-﻿
+﻿app.controller('CategoryController', ['$scope', '$http', 'CategoryMasterService', '$anchorScroll', 
+    function ($scope, $http, CategoryMasterService, $anchorScroll) {
+        debugger
+        $anchorScroll();
+        $('input').keypress(function (event) {
+            if (event.keyCode == 13) {
+                event.preventDefault();
+            }
+        });
+
+        $scope.Category = {};
+        $scope.btntextCategory = "Save";
+
+        $scope.ClearFilterData = function () {
+            $scope.filterText = "";
+            $scope.filterGrid();
+            $scope.CategoryDescription1 = false;
+        }
+
+        //Get Coutry Data into Grid
+        $scope.CategorygridOptions = {
+            dataSource: {
+                transport: {
+                    read: function (e) {
+                        debugger
+                        CategoryMasterService.GetCategory().success(function (data, status, headers, config) {
+                            debugger
+                            if (data != "") {
+                                $scope.data = JSON.parse(data);
+                                e.success($scope.data);
+                                $scope.dataReset = JSON.parse(data);
+                            }
+
+                        });
+                    }
+                },
+                pageSize: 10,
+                //serverPaging: true,
+                // serverSorting: true
+            },
+            sortable: true,
+            pageable: true,
+            selectable: "row",
+            columns: [
+                { field: "SrNo", title: "#", width: "50px" },
+                { field: "CategoryDescription", title: "Name", width: "150px" },
+                { field: "Active", title: "Active", width: "150px" },
+                { field: "SequenceNo", title: "Sequence", width: "150px" }
+            ]
+        };
+
+        //Save/Update Data Into Grid 
+        $scope.SaveCategory = function () {
+
+            var chkValFields = 0;
+            if ($scope.Category.CategoryDescription1 == "" || $scope.Category.CategoryDescription1 == undefined) {
+                $scope.CategoryDescription1 = true;
+                chkValFields = 1;
+            }
+
+            //if (chkValFields == 1) {
+            //    toaster.pop('warning', "warning", "Please re-check the input data and fill correctly");
+            //}
+
+            if (chkValFields == 0) {
+                $scope.Category.CategoryDescription = $scope.Category.CategoryDescription1;
+                $scope.Category.SequenceNo = $scope.Category.SequenceNo1;
+                $scope.Category.Active = $scope.Category.Active1;
+                if ($scope.Category.CategoryId) { $scope.Category.CategoryId = $scope.Category.CategoryId; }
+                CategoryMasterService.InsertCategory($scope.Category).success(function (data, status, headers, config) {
+                    $scope.CategoryDescription1 = false;
+
+                    $scope.OutputData = JSON.parse(data);
+                    if ($scope.OutputData == "1") {
+                        $scope.RefreshCategoryGrid();
+                        $scope.Category = {};
+                      //  toaster.pop('success', "Success", "Record Inserted Successfully");
+                        //alert('Record Insert.')
+                    }
+                    if ($scope.OutputData == "2") {
+                        $scope.RefreshCategoryGrid();
+                        $scope.Category = {};
+                      //  toaster.pop('success', "Success", "Record Updated Successfully");
+                        //alert('Record Update.')
+                    }
+                    if ($scope.OutputData == "0") {
+                       // toaster.pop('Exist', "Exist", "Record already Exists");
+                        //alert('Record already Exists.')
+                    }
+
+
+                });
+            }
+        }
+        //Fill Data into Controll while click on Grid for Update
+        $scope.onChangeCategoryGrid = function (selected, data, dataIteam, angularDataItem) {
+            $scope.CategoryDescription1 = false;
+            $scope.Category.CategoryDescription1 = data.CategoryDescription;
+            $scope.Category.SequenceNo1 = data.SequenceNo;
+            $scope.Category.Active1 = data.Active;
+            $scope.Category.CategoryId = data.CategoryId;
+            $scope.btntextCategory = "Update";
+        }
+        ///REFRESH GRID 
+        $scope.RefreshCategoryGrid = function () {
+            $scope.CategoryGridRebind = new kendo.data.DataSource({
+                transport: {
+                    read: {
+                        function(e) {
+
+                        }
+                    }
+                },
+            });
+        }
+
+
+        $scope.filterGrid = function () {
+
+            var grid = $scope.gridCategory;
+            if ($scope.filterText != "") {
+                grid.dataSource.query({
+                    page: 1,
+                    pageSize: 100,
+                    filter: {
+                        logic: "or",
+                        filters: [
+                            { field: "CategoryDescription", operator: "contains", value: $scope.filterText }
+                        ]
+                    }
+                });
+            }
+            else {
+                grid.dataSource.query({
+                    page: 1,
+                    pageSize: 10,
+                });
+            }
+        };
+
+
+        // clear fields
+        $scope.ClearCategoryFields = function () {
+
+            $scope.Category = {};
+            $scope.CategoryDescription1 = false;
+            $scope.btntextCategory = "Save";
+        }
+
+
+        //clear validations
+        $scope.ClearValMsgCategoryDescription1 = function () {
+            $scope.CategoryDescription1 = false;
+        }
+
+
+    }]);
