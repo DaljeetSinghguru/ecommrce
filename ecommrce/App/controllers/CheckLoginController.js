d1b440e5ff6c7e516ec95e49bd4d250dd6fd6798
daljeet <daljeetsinghguru@gmail.com>
2018-08-20 21:03:58 +0530

    changes for category subcategory
    changes for category subcategory

---
+++ ecommrce/App/controllers/CheckLoginController.js
@@ -0,0 +1,45 @@
+﻿app.controller('CheckLoginController', ['$scope', '$location', '$modal', '$http',
+    function ($scope, $location, $modal, $http) {
+        
+
+
+
+
+
+        $scope.GOTODestinationPage = function () {
+            debugger
+            $scope.Userlogin = {};
+            $scope.Userlogin.Name = $scope.sName;
+            $scope.Userlogin.Password = $scope.sUserPassword;
+            $http({ method: 'POST', url: $scope.Url + 'Login/checkLogin/', data: $scope.Userlogin }).
+                success(function (data, status, headers, config) {
+                    debugger
+                    if (data == "login successfully") {
+                        window.location.replace('app_v10.html#/LandingPageVisa');
+                    }
+                    else {
+                        alert("User Name and password is incorrect");
+                    }
+                    
+
+
+                }).
+                error(function (data, status, headers, config) {
+                });
+        }
+        $scope.cancel = function () {
+            //
+            if ($scope.modalInstanceExtention) {
+                $scope.modalInstanceExtention.dismiss('cancel');
+            }
+
+
+        }
+
+
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        //////////////////////////////////**********FORGET PASSWORD*****************///////////////////////////////
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    }]);
