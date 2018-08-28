dde5a07ffbd93d0af39ffa5c07089e7e3c8972c5
daljeet <daljeetsinghguru@gmail.com>
2018-08-28 07:20:23 +0530

    remove debugger

---
+++ ecommrce/obj/Release/Package/PackageTmp/App/Admin/Services/SubCategoryService.js
@@ -0,0 +1,14 @@
+﻿app.service('SubCategoryService', ['$http',  function ($http) {
+
+
+    this.baseURl = "http://localhost:50675/api/";
+
+
+    this.getData = function () {
+        return $http({ method: 'GET', url: this.baseURl + 'Subcategory/GetData' });
+    }
+    
+    this.getSubCategoryData = function (ID) {
+        return $http({ method: 'GET', url: this.baseURl + 'Subcategory/GetSubCategoryData?Id='+ID+'' });
+    }
+}])
---
+++ ecommrce/obj/Release/Package/PackageTmp/App/controllers/mainController2.js
@@ -0,0 +1,84 @@
+﻿app.controller('appmainController2', ['$scope', '$location', function ($scope,  $location) {
+    
+    //$scope.main = [];
+    //$scope.main.brand = 'QMS';
+    //$scope.main.name = 'Demo User';
+    
+
+
+    //$scope.IsAdmin = false;
+    //$scope.IsThermoFisherReportAdmin = false;
+
+    //$scope.UserName = "";
+    //$scope.IsLoggedIn = false;
+    //$scope.Token = "";
+    //$scope.menuVisible = true;
+    //$scope.mainclass = "col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main";
+
+    //$scope.isActive = function (viewLocation) {
+    //    var currentlocation = $location.path();
+    //    if (currentlocation == '/login') {
+    //        $scope.TurnMenuOff();
+    //    }
+    //    return viewLocation === currentlocation;
+    //};
+
+
+
+    //$scope.TurnMenuOn = function () {
+    //    if (!$scope.menuVisible) {
+    //        $scope.menuVisible = true;
+    //        $scope.mainclass = "col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main";
+    //    }
+    //}
+
+    //$scope.TurnMenuOff = function () {
+    //    if ($scope.menuVisible) {
+    //        $scope.menuVisible = false;
+    //        $scope.mainclass = "jumbotron";
+    //    }
+    //}
+
+
+    //$scope.ToggleMenu = function () {
+    //    $scope.menuVisible = !$scope.menuVisible;
+
+    //    if ($scope.menuVisible) {
+    //        $scope.mainclass = "col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main";
+    //    }
+    //    else {
+    //        $scope.mainclass = "jumbotron";
+    //    }
+    //}
+
+    //$scope.logout = function () {
+    //    $scope.TurnMenuOff();
+    //    $scope.UserName = "";
+    //    $scope.IsLoggedIn = false;
+    //    $scope.Token = "";
+    //    UserService.SetToken($scope.Token, $scope.IsLoggedIn, $scope.UserName);
+    //    $location.path('/login');
+    //}
+
+    //$scope.GetToken = function () {
+    //    $scope.IsLoggedIn = UserService.IsTokenSet();
+    //    $scope.Token = UserService.GetToken();
+    //    $scope.UserName = UserService.GetUserName();
+
+
+    //    $scope.main.name = UserService.GetUserName();
+    //    console.log("Is Logged In : " + $scope.IsLoggedIn);
+    //    if ($scope.IsLoggedIn == "false") {
+    //        console.log("Move to /login");
+    //        $location.path('/login');
+    //    }
+    //    else {
+    //        console.log("Currently Logged in");
+    //    }
+    //    return $scope.Token;
+    //}
+
+
+    ////$scope.GetToken();
+
+}]);
